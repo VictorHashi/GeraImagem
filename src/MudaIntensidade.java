4716000de179bae3ba077ef3a100e17cbabdac0e
VictorHashi <101532508+VictorHashi@users.noreply.github.com>
2024-11-01 04:21:45 -0300

    Conversor de intensidade (8bits para 5bits)

--- src/MudaIntensidade.java
+++ src/MudaIntensidade.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class Redimensiondor {
+public class MudaIntensidade {
 
     public static void main(String[] args) {
 
@@ -36,6 +36,8 @@ public class Redimensiondor {
                 }
             }
 
+
+
             int[][] image = new int[height][width];
             int row = 0;
             int col = 0;
@@ -54,72 +56,35 @@ public class Redimensiondor {
                 }
             }
 
+            reader.close();
+
             File dir = new File("Images" + File.separator + filename);
             if (!dir.exists()) {
                 dir.mkdirs();
             }
 
-            saveNewResize("10xMenor",filename, interpolar(image,image[0].length/10,image.length/10),maxValue);
-            saveNewResize("480x320",filename, interpolar(image,480,320),maxValue);
-            saveNewResize("1280x720",filename, interpolar(image,1280,720),maxValue);
-            saveNewResize("1920x1080",filename, interpolar(image,1920,1080),maxValue);
-            saveNewResize("3840x2160",filename, interpolar(image,3840,2160),maxValue);
-            saveNewResize("7680x4320",filename, interpolar(image,7680,4320),maxValue);
+            PrintWriter writer = new PrintWriter(new FileWriter("Images" + File.separator +
+                    filename + File.separator + filename + "5bits.pgm" ));
+
+            writer.println("P2");
+            writer.println(image[0].length + " " + image.length);
+            writer.println(31);
+
+            int[][] img5bit = new int[height][width];
+
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    img5bit[i][j] = (int) ((double)(image[i][j] * 31)/(double)255);
+                    writer.print(img5bit[i][j] + " ");
+                }
+                writer.println();
+            }
 
         } catch (Exception e) {
             System.out.println("Arquivo não encontrado ou fora do formato pgm.");
             throw new RuntimeException(e);
         }
 
-    }
-
-    public static void saveNewResize(String newName, String oldImage,int[][] image, int maxValue){
-        //Como o metodo de interpolação retorna apenas a matriz de valores da imagem,
-        // é necessário refazer o cabeçalho
-
-        try {
-            PrintWriter writer = new PrintWriter(new FileWriter("Images" + File.separator +
-                    oldImage + File.separator + newName + ".pgm"));
-
-            writer.println("P2");
-            writer.println(image[0].length + " " + image.length);
-            writer.println(maxValue);
-
-            for (int i = 0; i < image.length; i++) {
-                for (int j = 0; j < image[0].length; j++) {
-                    writer.print(image[i][j] + " ");
-                }
-                writer.println();
-            }
-
-            writer.close();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-
-    }
-
-    public static int[][] interpolar(int[][] image,
-                                     int newWidth, int newHeight){
-
-        int[][] newImage = new int[newHeight][newWidth];
-
-        for (int i = 0; i < newHeight; i++) {
-
-            for (int j = 0; j < newWidth; j++) {
-
-                int x = (int) ((j/(double)newWidth) * image[0].length);
-                int y = (int) ((i/(double)newHeight) * image.length);
-
-                newImage[i][j] = image[y][x];
-
-            }
-
-        }
-
-        return newImage;
     }
 
 }
